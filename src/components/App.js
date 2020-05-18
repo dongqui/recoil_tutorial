7ad251d41c195e78fb6828e1a21c0e3ee2ca078e
Unknown <kimwinner90@gmail.com>
2020-05-18 14:36:37 +0900

    init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CharacterCounter from './CharacterCounter';
+import TextInput from './TextInput';
+
+function App() {
+  return (
+    <div className="App">
+      <TextInput />
+      <CharacterCounter />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 );
